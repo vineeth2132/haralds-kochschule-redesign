a1107b53074523a5b9a80de3022bc100ce8135ed
vineeth2132 <vineeth2320@gmail.com>
2026-04-28 06:17:30 +0200

    fixed minor bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
+
+import ScrollToTop from "./components/ScrollToTop";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import StandardPage from "./pages/StandardPage";
+import Media from "./pages/Media";
+
 import { pageData } from "./data/pages";
-import Media from "./pages/media";
 
 function App() {
   const [language, setLanguage] = useState("en");
 
   return (
     <>
+      <ScrollToTop />
+
       <Navbar language={language} setLanguage={setLanguage} />
 
       <Routes>
